ac148ea0d15800204e62e94abb775dabbfa71de7
[aslan-asilon3] <[sulaslansetiawan3@gmail.com]>
2022-06-08 18:00:39 +0800

    Ad some features Variables and Data Type

---
+++ 1VariablesAndDataTypes/2CheckVariableValue/issetFunction.php
@@ -0,0 +1,18 @@
+<?php 
+// isset akan mengembalikan nilai true jika 
+// variabel sudah didefinisikan dan akan
+// mengembalikan nilai false jika variabel
+// belum di definisikan
+
+$a = 10;
+echo isset($a) ? "True" : "False";
+echo "\n";
+echo isset($b) ? "True" : "False";
+
+
+
+
+
+
+
+?>
